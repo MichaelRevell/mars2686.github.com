f762a43f225cb38cdd8a4cb4d3be772ad6102248
Michael <mars8082686@gmail.com>
2012-01-09 19:59:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mars2686::Application.config.secret_token = '933c5d042e186cae9fda2b2dda6554554de622732a67a383a047118ca1b78d17b2c47387790a0de3025531cf95dfec99d03998f3048c02f09b0b78856734f9dc'
